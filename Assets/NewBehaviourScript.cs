88aec512f1f7e783bbaffbe60eef100e3c1f564a
user <https://github.com/Rita1114/0706.git>
2022-07-06 11:01:30 +0800

    0706BUtton測試

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,13 +2,28 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    public Button btn;
+
+    public Image a;
     // Start is called before the first frame update
     void Start()
     {
         Console.WriteLine("測試123");
+        
+        btn.onClick.AddListener(OnbtnClick);
+    }
+     
+    public void OnbtnClick()
+    {
+        a.color = new Color(255, 255, 255);
+        a.transform.localScale =new Vector3(1, 3, 5);
+        a.transform.localPosition = new Vector3(50, 50, 50);
+
+
     }
 
     // Update is called once per frame
